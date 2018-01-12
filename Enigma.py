6f0ed7b65b5e8f50d9347c1b697975524736906e
Archan Patkar <archanpatkar@gmail.com>
2018-01-13 02:33:12 +0530

    Update

--- Enigma.py
+++ Enigma.py
@@ -1,14 +1,9 @@
 from Rotor import Rotor
 
 
-r = Rotor(21);
-r1 = Rotor(5);
-r2 = Rotor(7);
+r = Rotor(5);
 
 input = "a";
 
-print(r.step());
-print(r1.step());
-print(r2.step());
-
+print(r.scramble(input));
 
