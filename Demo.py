f2fb6271a96f9c3eef0b26f61ecd6d194d535998
Archan Patkar <archanpatkar@gmail.com>
2018-01-13 20:49:24 +0530

    Update

--- Demo.py
+++ Demo.py
@@ -3,8 +3,10 @@ from Enigma import Enigma
 
 e1 = Enigma(
             {
-                "I":10,
-                "II":0,
-                "III":9
+                "I":(10,"1"),
+                "II":(0,"1"),
+                "III":(9,"1")
             }
         );
+
+e1.encrypt("");
