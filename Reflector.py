12f09d619950785b7296d61d96700b2c1bbc2713
Archan Patkar <archanpatkar@gmail.com>
2018-01-14 01:01:53 +0530

    Update

--- Reflector.py
+++ Reflector.py
@@ -1,7 +1,32 @@
-class Reflector():
-    def __init__(self):
-        
-        pass;
+# class Reflector():
+#     def __init__(self):
+#         pass;
 
-    
-
+B = {
+    "a":"y",
+    "b":"r",
+    "c":"u",
+    "d":"h",
+    "e":"q",
+    "f":"s",
+    "g":"l",
+    "h":"d",
+    "i":"p",
+    "j":"x",
+    "k":"n",
+    "l":"g",
+    "m":"o",
+    "n":"k",
+    "o":"m",
+    "p":"i",
+    "q":"e",
+    "r":"b",
+    "s":"f",
+    "t":"z",
+    "u":"c",
+    "v":"w",
+    "w":"v",
+    "x":"j",
+    "y":"a",
+    "z":"t"
+}
