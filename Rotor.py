e994bb91bc82634b419f749b7a5a3c2aec709e3e
Archan Patkar <archanpatkar@gmail.com>
2018-01-14 14:42:15 +0530

    Update

--- Rotor.py
+++ Rotor.py
@@ -3,35 +3,35 @@ from Rotor_Types import *
 
 class Rotor(EventEmitter):
     letters = [
-            "a",
-            "b",
-            "c",
-            "d",
-            "e",
-            "f",
-            "g",
-            "h",
-            "i",
-            "j",
-            "k",
-            "l",
-            "m",
-            "n",
-            "o",
-            "p",
-            "q",
-            "r",
-            "s",
-            "t",
-            "u",
-            "v",
-            "w",
-            "x",
-            "y",
-            "z"
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"
     ];
 
-    def __init__(self,initial=0,rtype="1"):
+    def __init__(self,initial=0,rtype="I"):
         EventEmitter.__init__(self);
         self.rotor = initial;
         letters = rotors.get(rtype);
