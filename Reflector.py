38de8c3baaee3b7aadbac094f98f3f2ffc432b6e
Archan Patkar <archanpatkar@gmail.com>
2018-01-15 21:03:08 +0530

    Update

--- Reflector.py
+++ Reflector.py
@@ -88,11 +88,37 @@ rf_types = {
             "X":"J",
             "Y":"A",
             "Z":"T"
+        },
+
+        "Mirror" : {
+            "A":"A",
+            "B":"B",
+            "C":"C",
+            "D":"D",
+            "E":"E",
+            "F":"F",
+            "G":"G",
+            "H":"H",
+            "I":"I",
+            "J":"J",
+            "K":"K",
+            "L":"L",
+            "M":"M",
+            "N":"N",
+            "O":"O",
+            "P":"P",
+            "Q":"Q",
+            "R":"R",
+            "S":"S",
+            "T":"T",
+            "U":"U",
+            "V":"V",
+            "W":"W",
+            "X":"X",
+            "Y":"Y",
+            "Z":"Z"
         }
 }
-
-
-
 
 def reflector(type):
     return rf_types.get(type);
