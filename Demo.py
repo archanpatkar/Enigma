e47ebf5c7fbe23a952cee311d349b669305918f9
Archan Patkar <archanpatkar@gmail.com>
2018-01-15 21:02:52 +0530

    Update

--- Demo.py
+++ Demo.py
@@ -2,42 +2,24 @@ from Enigma import Enigma
 
 e1 = Enigma(
             rotors = [
-                        (0,"IC"),
-                        (13,"IIC"),
-                        (5,"IIIC")
+                        (21,"IC"),
+                        (9,"IIC"),
+                        (13,"IIIC")
             ],
             reflector = "B"
         );
 
-encrypted_string = e1.encrypt("Hello")
+encrypted_string = e1.encrypt("HELLO")
 print(encrypted_string);
-
-# print(e1.I.scramble("A"))
-# print(e1.I.unscramble("D"))
-
-# print(e1.II.scramble("A"))
-# print(e1.II.unscramble("H"))
-
-# print(e1.III.scramble("A"))
-# print(e1.III.unscramble("U"))
-
-# print(e1.each("A"));
-
-# print(e1.each("A"))
-# print(e1.eachinv("Z"))
-# print(e1.eachinv(""))
-
 
 
 e2 = Enigma(
             rotors = [
-                        (0,"IC"),
-                        (13,"IIC"),
-                        (5,"IIIC")
+                        (21,"IC"),
+                        (9,"IIC"),
+                        (13,"IIIC")
             ],
             reflector = "B"
         );
 
-# print(e2.eachinv("Z"))
 print(e2.decrypt(encrypted_string));
-# print("out ->",e2.each("U"))
