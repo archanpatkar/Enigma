a45203685298d4f15b2421df841b681c50e93d2f
Archan Patkar <archanpatkar@gmail.com>
2018-01-16 03:27:12 +0530

    Update

--- Demo.py
+++ Demo.py
@@ -2,23 +2,23 @@ from Enigma import Enigma
 
 e1 = Enigma(
             rotors = [
-                        (21,"IC"),
-                        (9,"IIC"),
-                        (13,"IIIC")
-            ],
+                        (0,"IC"),
+                        (0,"IIC"),
+                        (0,"IIIC"),
+                     ],
             reflector = "B"
         );
 
-encrypted_string = e1.encrypt("HELLO")
+encrypted_string = e1.encrypt("Hello")
 print(encrypted_string);
 
 
 e2 = Enigma(
             rotors = [
-                        (21,"IC"),
-                        (9,"IIC"),
-                        (13,"IIIC")
-            ],
+                        (0,"IC"),
+                        (0,"IIC"),
+                        (0,"IIIC"),
+                     ],
             reflector = "B"
         );
 
