15c8508501d2fd4a1f693073365214b4f8b03f2d
Archan Patkar <archanpatkar@gmail.com>
2018-01-16 17:30:28 +0530

    Update

--- Reflector.py
+++ Reflector.py
@@ -2,7 +2,9 @@
 #     def __init__(self):
 #         pass;
 
-rf_types = 
+import json
+
+rf_types =  json.load(open("./reflectors.json",mode="r"))
 
 def reflector(type):
     return rf_types.get(type);
